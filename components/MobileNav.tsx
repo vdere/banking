525e90cf6950f9cdab2f83e2cbb467e6625620e9
Viraj Dere <virajdere@Virajs-Mac-mini.local>
2024-12-31 15:45:19 +0530

    Commit for components/MobileNav.tsx

---
+++ components/MobileNav.tsx
@@ -0,0 +1,94 @@
+"use client";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const MobileNav = ({ user }: MobileNavProps) => {
+  const pathName = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            className="cursor-pointer"
+            width={30}
+            height={30}
+            alt="menu"
+          />
+        </SheetTrigger>
+        <SheetContent className="border-none bg-white" side="left">
+        <SheetTitle>Menu</SheetTitle> 
+          <Link
+            href="/"
+            className="cursor-pointer items-center gap-1 flex px-4"
+          >
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Horizon logo"
+            ></Image>
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              Horizon
+            </h1>
+          </Link>
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathName === item.route ||
+                    pathName.startsWith(`${item.route}/`);
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        className={cn("mobile-nav-sheet_close w-full", {
+                          "bg-bank-gradient": isActive,
+                        })}
+                        href={item.route}
+                        key={item.label}
+                      >
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        ></Image>
+                        <p
+                          className={cn("text-16 font-semibold text-black-2", {
+                            "text-white": isActive,
+                          })}
+                        >
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+                USER
+              </nav>
+            </SheetClose>
+            Footer
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;

2dcfdde996fae42064e38db1c984bd8f0ffd58ed
Viraj Dere <mjviraj123@gmail.com>
2025-01-02 18:13:13 +0530

    updated page

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,19 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // mutation / database / make fetch
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+      // mutation / database / make fetch
+      // Create a user account
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+  
